16e50f375518abd31a789c3bace91d236db0f0dc
NITHIN D NAIR <nithindivakarannair92@gmail.com>
2024-06-09 00:16:38 +0530

    a2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        // Change the target to your new deployed URL
-        target: "https://www.threadd.xyz",
-        changeOrigin: true,
-        secure: true,  // Since you're using HTTPS, this should be true
-      },
-    },
-  },
-})
+
+})  
+ 
+ 
