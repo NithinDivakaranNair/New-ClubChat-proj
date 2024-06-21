7a0e3e7d4b6bc016fbfc690bf7fce3d828e074bb
NITHIN D NAIR <nithindivakarannair92@gmail.com>
2024-06-22 01:50:02 +0530

    last1

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -7,7 +7,30 @@ import { createPost,
           replyToPost,
           getFeedPosts,
           getUserPosts } from "../controllers/postController.js";
-import protectRoute from "../middlewares/protectRoute.js";
+
+router.get("/feed", getFeedPosts);
+router.post('/create', createPost)
+router.get("/:id",getPost)
+router.get("/user/:username",getUserPosts);
+router.delete("/:id",deletePost)
+router.put("/like/:id",likeUnlikePost)
+router.put("/reply/:id",replyToPost)
+
+export default router;
+
+
+
+
+// import express from "express"
+// const  router=express.Router();
+// import { createPost,
+//           getPost,
+//           deletePost,
+//           likeUnlikePost,
+//           replyToPost,
+//           getFeedPosts,
+//           getUserPosts } from "../controllers/postController.js";
+// import protectRoute from "../middlewares/protectRoute.js";
 
 // router.get("/feed",protectRoute, getFeedPosts);
 // router.post('/create',protectRoute, createPost)
@@ -17,12 +40,4 @@ import protectRoute from "../middlewares/protectRoute.js";
 // router.put("/like/:id",protectRoute,likeUnlikePost)
 // router.put("/reply/:id",protectRoute,replyToPost)
 
-router.get("/feed",getFeedPosts);
-router.post('/create',createPost)
-router.get("/:id",getPost)
-router.get("/user/:username",getUserPosts);
-router.delete("/:id",deletePost)
-router.put("/like/:id",likeUnlikePost)
-router.put("/reply/:id",replyToPost)
-
-export default router;
+// export default router;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,6 @@
 import express from "express";
 const router = express.Router();
 
-import protectRoute from "../middlewares/protectRoute.js";
 
 import { signupUser,
          loginUser,
@@ -14,17 +13,52 @@ import { signupUser,
 
 // router.get('/profile/:username', getUserProfile);
 router.get('/profile/:query', getUserProfile);
-router.get("/suggested" ,protectRoute,getSuggestedUsers)
+router.get("/suggested" ,getSuggestedUsers)
 
 router.post('/signup', signupUser);
 router.post('/login', loginUser);
 router.post('/logout', logoutUser);
-// router.post('/follow/:id',protectRoute, followUnFollowUser);
-// router.put('/update/:id',protectRoute, updateUser);
-// // router.get('/profile/:id', getUserProfile);
-// router.put("/freeze",protectRoute,freezeAccount);
 router.post('/follow/:id', followUnFollowUser);
 router.put('/update/:id', updateUser);
 // router.get('/profile/:id', getUserProfile);
 router.put("/freeze",freezeAccount);
+
 export default router;
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// const router = express.Router();
+
+// import protectRoute from "../middlewares/protectRoute.js";
+
+// import { signupUser,
+//          loginUser,
+//          logoutUser,
+//          followUnFollowUser,
+//          updateUser,
+//          getUserProfile,
+//          freezeAccount,
+//          getSuggestedUsers } from "../controllers/userController.js";
+
+// // router.get('/profile/:username', getUserProfile);
+// router.get('/profile/:query', getUserProfile);
+// router.get("/suggested" ,protectRoute,getSuggestedUsers)
+
+// router.post('/signup', signupUser);
+// router.post('/login', loginUser);
+// router.post('/logout', logoutUser);
+// router.post('/follow/:id',protectRoute, followUnFollowUser);
+// router.put('/update/:id',protectRoute, updateUser);
+// // router.get('/profile/:id', getUserProfile);
+// router.put("/freeze",protectRoute,freezeAccount);
+
+// export default router;
